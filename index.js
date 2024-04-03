eb6da7bbeea72c8931392c924a512634ade5574b
Brian Ngetich <kipronobrian97@gmail.com>
2024-04-03 11:01:52 +0300

    Filters Added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array,matched){
+   return array.filter(
+    (driver) => driver.toUpperCase() === matched.toUpperCase());
+}
+
+function fuzzyMatch(array,matched){
+    return array.filter(
+        (pMatch) => pMatch.toUpperCase().indexOf(matched.toUpperCase())===0
+    );
+}
+function matchName(array,matched){
+    return array.filter(
+        (driver) => driver.name === matched
+    );
+}
